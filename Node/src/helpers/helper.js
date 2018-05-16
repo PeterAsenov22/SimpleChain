5c450e241bc5d76e75f30e95a172461eca3d6c0c
PeterAsenov22 <peter12345@abv.bg>
2018-05-16 19:39:50 +0300

    new helper functions added

--- Node/src/helpers/helper.js
+++ Node/src/helpers/helper.js
@@ -1,30 +1,22 @@
+const EllipticCurve = require('elliptic').ec
+const secp256k1 = new EllipticCurve('secp256k1')
+const CryptoJs = require('crypto-js')
 const Block = require('../Block').Block
 const Transaction = require('../Transaction')
-
-const faucetPrivateKey = 'd816f0c72ecc7dc1f0a8d5294676098ea0d5ee826f5c13c7026952ff8ea24fe4'
-const nullAddress = '0000000000000000000000000000000000000000'
-const nullPubKey = '00000000000000000000000000000000000000000000000000000000000000000'
-const nullSignature = [
-  '0000000000000000000000000000000000000000000000000000000000000000',
-  '0000000000000000000000000000000000000000000000000000000000000000'
-]
-const networkCirculatingSupply = 1000000000000
-
-function privateKeyToAddress (privateKey) {
-}
+const nodeConfig = require('../config/node')
 
 function generateGenesisBlock () {
-  let faucetAddress = privateKeyToAddress(faucetPrivateKey)
+  let faucetAddress = privateKeyToAddress(nodeConfig.faucetPrivateKey)
   let genesisTimestamp = Date.now()
 
   let genesisTransaction = new Transaction(
-    nullAddress,
+    nodeConfig.nullAddress,
     faucetAddress,
-    networkCirculatingSupply,
+    nodeConfig.networkCirculatingSupply,
     0,
     genesisTimestamp,
-    nullPubKey,
-    nullSignature,
+    nodeConfig.nullPubKey,
+    nodeConfig.nullSignature,
     undefined,
     0,
     true
@@ -35,7 +27,7 @@ function generateGenesisBlock () {
     [genesisTransaction],
     0,
     undefined,
-    nullAddress,
+    nodeConfig.nullAddress,
     undefined,
     0,
     genesisTimestamp,
@@ -45,7 +37,107 @@ function generateGenesisBlock () {
   return genesisBlock
 }
 
+function privateKeyToPublicKey (privateKey) {
+  let keyPair = secp256k1.keyFromPrivate(privateKey)
+  let isOdd = keyPair.getPublic().getY().isOdd()
+  let publicKey = keyPair.getPublic().getX() + (isOdd ? '1' : '0')
+  return publicKey
+}
+
+function publicKeyToAddress (publicKey) {
+  return CryptoJs.RIPEMD160(publicKey).toString()
+}
+
+function privateKeyToAddress (privateKey) {
+  let publicKey = privateKeyToPublicKey(privateKey)
+  let address = publicKeyToAddress(publicKey)
+  return address
+}
+
+function isValidAddress (address) {
+  return /^[0-9a-f]{40}$/.test(address)
+}
+
+function isValidPublicKey (publicKey) {
+  return /^[0-9a-f]{65}$/.test(publicKey)
+}
+
+function isValidTransferAmount (amount) {
+  if (typeof (amount) !== 'number') {
+    return false
+  }
+  if (!Number.isInteger(amount)) {
+    return false
+  }
+
+  return (amount >= 0) && (amount <= nodeConfig.networkCirculatingSupply)
+}
+
+function isValidFee (fee) {
+  if (typeof (fee) !== 'number') {
+    return false
+  }
+  if (!Number.isInteger(fee)) {
+    return false
+  }
+
+  return fee >= nodeConfig.minTransactionFee
+}
+
+function isValidTimestamp (date) {
+  const isoTimeRegex =
+    /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{2,6}Z$/
+
+  if (typeof (date) !== 'string') {
+    return false
+  }
+  if (!isoTimeRegex.test(date)) {
+    return false
+  }
+
+  let d = Date.parse(date)
+  if (Number.isNaN(d)) {
+    return false
+  }
+
+  let year = d.getUTCFullYear()
+  return (year >= 2018) && (year <= 2100)
+}
+
+function isValidSignatureFormat (signature) {
+  if (!Array.isArray(signature)) {
+    return false
+  }
+  if (signature.length !== 2) {
+    return false
+  }
+
+  return /^[0-9a-f]{1,65}$/.test(signature)
+}
+
+function sign (data, privateKey) {
+  let keyPair = secp256k1.keyFromPrivate(privateKey)
+  let signature = keyPair.sign(data)
+  return [signature.r.toString(16), signature.s.toString(16)]
+}
+
+function validateSignature (data, publicKey, signature) {
+  let keyPair = secp256k1.keyFromPublic(publicKey, 'hex')
+  return keyPair.verify(data, {r: signature[0], s: signature[1]})
+}
+
+
 module.exports = {
   generateGenesisBlock: generateGenesisBlock,
-  privateKeyToAddress: privateKeyToAddress
+  privateKeyToPublicKey: privateKeyToPublicKey,
+  publicKeyToAddress: publicKeyToAddress,
+  privateKeyToAddress: privateKeyToAddress,
+  isValidAddress: isValidAddress,
+  isValidPublicKey: isValidPublicKey,
+  isValidFee: isValidFee,
+  isValidTransferAmount: isValidTransferAmount,
+  isValidSignatureFormat: isValidSignatureFormat,
+  isValidTimestamp: isValidTimestamp,
+  sign: sign,
+  validateSignature: validateSignature
 }
