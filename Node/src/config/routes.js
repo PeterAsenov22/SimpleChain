3f07d82c8b4e45c49a924c976c2c583ff34b4797
PeterAsenov22 <peter12345@abv.bg>
2018-05-29 14:13:35 +0300

    implemented P2P communication

--- Node/src/config/routes.js
+++ Node/src/config/routes.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const node = require('../Node').Node
 const HttpStatus = require('http-status-codes')
 
@@ -14,6 +15,8 @@ module.exports = (app) => {
   app.get('/info', (req, res) => {
     res.json({
       'about': 'SimpleChain/0.1-js',
+      'nodeId': node.nodeId,
+      'chainId': node.chainId,
       'nodeUrl': node.selfUrl,
       'peers': node.peers.length,
       'difficulty': node.chain.difficulty,
@@ -74,6 +77,31 @@ module.exports = (app) => {
       res.status(HttpStatus.BAD_REQUEST).json(sendResult)
     }
   })
+  app.get('/mining/get-mining-job/:address', (req, res) => {
+    let address = req.params.address
+    let blockCandidate = node.chain.getMiningJob(address)
+    res.json({
+      index: blockCandidate.index,
+      transactionsIncluded: blockCandidate.transactions.length,
+      difficulty: blockCandidate.difficulty,
+      expectedReward: blockCandidate.transactions[0].amount,
+      rewardAddress: blockCandidate.transactions[0].recipientAddress,
+      blockDataHash: blockCandidate.dataHash
+    })
+  })
+  app.post('/mining/submit-mined-block', (req, res) => {
+    let dataHash = req.body.dataHash
+    let timestamp = req.body.timestamp
+    let nonce = req.body.nonce
+    let blockHash = req.body.blockHash
+    let result = node.chain.submitMinedBlock(dataHash, timestamp, nonce, blockHash)
+    if (result.errorMsg) {
+      res.status(HttpStatus.BAD_REQUEST).json(result)
+    } else {
+      res.json({'message': `Block accepted, reward paid: ${result.transactions[0].amount} microcoins`})
+      node.notifyPeersAboutNewBlock()
+    }
+  })
   app.get('/debug/mine/:minerAddress/:difficulty', (req, res) => {
     let minerAddress = req.params.minerAddress
     let difficulty = parseInt(req.params.difficulty) || 3
@@ -85,5 +113,41 @@ module.exports = (app) => {
   })
   app.get('/peers', (req, res) => {
     res.json(node.peers)
+  })
+  app.post('/blocks/notify-new-block', (req, res) => {
+    node.syncChain(req.body)
+    res.json({ message: 'Thank you for the notification.' })
+  })
+  app.post('/peers/connect', (req, res) => {
+    let peerUrl = req.body.peerUrl
+    if (peerUrl === undefined) {
+      return res.status(HttpStatus.BAD_REQUEST.json({errorMsg: "Missing 'peerUrl' in the request body"}))
+    }
+    console.log('Trying to connect to peer: ' + peerUrl)
+    axios.get(peerUrl + '/info')
+      .then(function (result) {
+        if (node.peers[result.data.nodeId]) {
+          console.log('Error: already connected to peer: ' + peerUrl)
+          return res.status(HttpStatus.CONFLICT).json({errorMsg: 'Already connected to peer: ' + peerUrl})
+        } else {
+          node.peers[result.data.nodeId] = peerUrl
+          console.log('Successfully connected to peer: ' + peerUrl)
+          node.syncChain(result.data)
+          res.json({message: 'Connected to peer: ' + peerUrl})
+
+          // Try to connect back the remote peer to self
+          axios.post(peerUrl + '/peers/connect', {peerUrl: node.selfUrl})
+            .then(function () {
+
+            })
+            .catch(function () {
+
+            })
+        }
+      })
+      .catch(function () {
+        console.log(`Error: connecting to peer: ${peerUrl} failed.`)
+        return res.status(HttpStatus.BAD_REQUEST).json({errorMsg: 'Cannot connect to peer: ' + peerUrl})
+      })
   })
 }
